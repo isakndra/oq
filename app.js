9273fcad9c34668bbe6c0372fee54b06537151d8
isakndra <isakndar001@gmail.com>
2025-07-22 11:41:14 +0500

    Update app.js
    11111111

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.listen(PORT, () => {
 
 async function checkLock() {
   try {
-    const res = await axios.get(`http://localhost:${PORT}/check-lock`, {
+    const res = await axios.get(`https://oq-ysum.onrender.com/check-lock`, {
       headers: {
         'User-Agent': HOSTNAME
       }
